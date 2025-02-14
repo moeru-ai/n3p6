1399b79f4e2ff27fd03ce8f50d1e722588582bae
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-02-14 19:12:37 +0800

    fix(app): remove drei loader

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,6 +1,5 @@
 import { Routes } from '@generouted/react-router/lazy'
-import { Loader } from '@react-three/drei'
-import { StrictMode, Suspense } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import './main.css'
@@ -8,9 +7,6 @@ import './main.css'
 // eslint-disable-next-line @masknet/no-top-level
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Suspense fallback={null}>
-      <Routes />
-    </Suspense>
-    <Loader />
+    <Routes />
   </StrictMode>,
 )
