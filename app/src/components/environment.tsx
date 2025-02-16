1657606ab82c683b231485462f8a09bad4e5d1a7
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-02-16 14:26:26 +0800

    fix(environment): move ambient light

--- app/src/components/environment.tsx
+++ app/src/components/environment.tsx
@@ -4,31 +4,35 @@ import { isDarkMode } from '@react-three/uikit'
 import { colors } from '@react-three/uikit-default'
 import { IfInSessionMode } from '@react-three/xr'
 
-export const Environment = () => (
-  <>
-    <RigidBody includeInvisible type="fixed">
-      <mesh
-        position={[0, 0, 0]}
-        scale={[100, 0.1, 100]}
-        visible={false}
-      >
-        <boxGeometry />
-      </mesh>
-    </RigidBody>
-    <IfInSessionMode deny="immersive-ar">
-      <Grid
-        cellColor={colors.foreground.value}
-        cellSize={3}
-        cellThickness={1}
-        fadeDistance={100}
-        fadeStrength={5}
-        infiniteGrid
-        position={[0, 0, 0]}
-        sectionSize={0}
-      />
-      <ambientLight intensity={0.5} />
-      <color args={[colors.background.value]} attach="background" />
-      {isDarkMode.value && <Stars count={99} depth={99} fade />}
-    </IfInSessionMode>
-  </>
-)
+export const Environment = () => {
+  const intensity = isDarkMode.value ? 0.5 : 1
+
+  return (
+    <>
+      <ambientLight intensity={intensity} />
+      <RigidBody includeInvisible type="fixed">
+        <mesh
+          position={[0, 0, 0]}
+          scale={[100, 0.1, 100]}
+          visible={false}
+        >
+          <boxGeometry />
+        </mesh>
+      </RigidBody>
+      <IfInSessionMode deny="immersive-ar">
+        <Grid
+          cellColor={colors.foreground.value}
+          cellSize={3}
+          cellThickness={1}
+          fadeDistance={100}
+          fadeStrength={5}
+          infiniteGrid
+          position={[0, 0, 0]}
+          sectionSize={0}
+        />
+        <color args={[colors.background.value]} attach="background" />
+        {isDarkMode.value && <Stars count={99} depth={99} fade />}
+      </IfInSessionMode>
+    </>
+  )
+}
