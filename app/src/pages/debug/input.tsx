e06eaf7164cffac4c54f85ab1c4fb88ee8d55460
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-05-04 15:41:57 +0800

    chore(app): update debug input

--- app/src/pages/debug/input.tsx
+++ app/src/pages/debug/input.tsx
@@ -1,15 +1,33 @@
-import { Root } from '@react-three/uikit'
-import { Defaults, Input } from '@react-three/uikit-default'
+import { Container, Root, Text } from '@react-three/uikit'
+import { Button, Defaults, Input } from '@react-three/uikit-default'
+import { useState } from 'react'
 
 import { Stage } from '~/components/stage'
 
 const DebugInput = () => {
+  const [value, setValue] = useState<string>()
+
   return (
     <Stage>
       <group position={[0, 1, 0]}>
         <Root>
           <Defaults>
-            <Input placeholder="Write a message..." width={200} />
+            <Container flexDirection="column" gap={4}>
+              <Input
+                data-test-id="debug-input"
+                onValueChange={value => setValue(value)}
+                placeholder="Write a message..."
+                value={value}
+                width={200}
+              />
+              <Button
+                data-test-id="debug-button"
+                onClick={() => setValue(undefined)}
+                variant="secondary"
+              >
+                <Text>Submit</Text>
+              </Button>
+            </Container>
           </Defaults>
         </Root>
       </group>
