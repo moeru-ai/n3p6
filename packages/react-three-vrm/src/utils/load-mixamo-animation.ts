49b375545b228984932a73bf07dced5a20f30b38
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-05-24 14:55:52 +0800

    chore(react-three-vrm)!: remove mixamo

--- packages/react-three-vrm/src/utils/load-mixamo-animation.ts
+++
@@ -1,8 +0,0 @@
-import type { VRM } from '@pixiv/three-vrm'
-
-import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader.js'
-
-import { createMixamoAnimationClip } from './create-mixamo-animation-clip'
-
-export const loadMixamoAnimation = async (url: string, vrm: VRM) =>
-  createMixamoAnimationClip(await new FBXLoader().loadAsync(url), vrm)
