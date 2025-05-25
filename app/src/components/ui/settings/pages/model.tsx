25bf988a488df970e4a970e714bd4184c29bcf7c
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-05-25 12:10:01 +0800

    chore(app/settings/model): update ui

--- app/src/components/ui/settings/pages/model.tsx
+++ app/src/components/ui/settings/pages/model.tsx
@@ -1,17 +1,15 @@
 import type { ContainerProperties } from '@react-three/uikit'
 
 import { Container, Text } from '@react-three/uikit'
-import { Button } from '@react-three/uikit-default'
+import { Button, Card, CardContent, CardDescription, CardHeader, CardTitle } from '@react-three/uikit-default'
 import { FileUpIcon, RotateCcwIcon } from '@react-three/uikit-lucide'
 import { useCallback, useEffect, useMemo } from 'react'
 
 import { useModelStore } from '~/hooks/use-model-store'
 
 export const SettingsModel = (props: ContainerProperties) => {
-  const { resetModel, setModel } = useModelStore(({ resetModel, setModel }) => ({
-    resetModel,
-    setModel,
-  }))
+  const resetModel = useModelStore(({ resetModel }) => resetModel)
+  const setModel = useModelStore(({ setModel }) => setModel)
 
   const handleFileUpload = useCallback(async (event: Event) => {
     const { files } = event?.target as HTMLInputElement
@@ -52,14 +50,26 @@ export const SettingsModel = (props: ContainerProperties) => {
 
   return (
     <Container flexDirection="column" gap={16} overflow="scroll" padding={16} {...props}>
-      <Button data-test-id="upload-model" gap={8} onClick={() => input.click()}>
-        <FileUpIcon height={16} width={16} />
-        <Text>Upload Model</Text>
-      </Button>
-      <Button data-test-id="reset-model" gap={8} onClick={resetModel} variant="destructive">
-        <RotateCcwIcon height={16} width={16} />
-        <Text>Reset Model</Text>
-      </Button>
+      <Card height="auto">
+        <CardHeader>
+          <CardTitle>
+            <Text>Change Model</Text>
+          </CardTitle>
+          <CardDescription>
+            {/* <Text>Generates audio from the input text.</Text> */}
+          </CardDescription>
+        </CardHeader>
+        <CardContent flexDirection="column" gap={16}>
+          <Button data-test-id="upload-model" gap={8} onClick={() => input.click()}>
+            <FileUpIcon height={16} width={16} />
+            <Text>Upload Model</Text>
+          </Button>
+          <Button data-test-id="reset-model" gap={8} onClick={resetModel} variant="destructive">
+            <RotateCcwIcon height={16} width={16} />
+            <Text>Reset Model</Text>
+          </Button>
+        </CardContent>
+      </Card>
     </Container>
   )
 }
