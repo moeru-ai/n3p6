f9591fdb901b76682b31d4d349860f852fe39afa
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-05-26 23:15:29 +0800

    docs: update

--- docs/astro.config.ts
+++ docs/astro.config.ts
@@ -1,5 +1,6 @@
 import starlight from '@astrojs/starlight'
 import { defineConfig } from 'astro/config'
+import starlightSidebarTopics from 'starlight-sidebar-topics'
 import starlightThemeNova from 'starlight-theme-nova'
 
 // https://astro.build/config
@@ -7,6 +8,9 @@ export default defineConfig({
   base: '/docs',
   integrations: [
     starlight({
+      components: {
+        Sidebar: './src/components/Sidebar.astro',
+      },
       editLink: {
         baseUrl: 'https://github.com/moeru-ai/chat/edit/main/docs',
       },
@@ -16,13 +20,30 @@ export default defineConfig({
         // replacesTitle: true,
         src: './public/favicon.png',
       },
-      plugins: [starlightThemeNova()],
-      sidebar: [
-        {
-          autogenerate: { directory: 'packages' },
-          label: 'Packages',
-        },
+      plugins: [
+        starlightThemeNova(),
+        starlightSidebarTopics([
+          {
+            icon: 'open-book',
+            items: [{ autogenerate: { directory: 'guides' }, label: 'Guides' }],
+            label: 'Guides',
+            link: 'guides/',
+          },
+          {
+            icon: 'pencil',
+            items: [{ autogenerate: { directory: 'contributing' }, label: 'Contributing' }],
+            label: 'Contributing',
+            link: 'contributing/',
+          },
+          {
+            icon: 'npm',
+            items: [{ autogenerate: { directory: 'packages' }, label: 'Packages' }],
+            label: 'Packages',
+            link: 'packages/react-three-vrm',
+          },
+        ]),
       ],
+
       social: [{ href: 'https://github.com/moeru-ai/chat', icon: 'github', label: 'GitHub' }],
       title: 'moeChat Docs',
     }),
