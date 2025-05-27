6489781e0d04ae53eeb7df9e680f20b37166bde8
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-05-27 17:20:16 +0800

    chore(app): expression demo

--- app/src/router.ts
+++ app/src/router.ts
@@ -5,6 +5,7 @@ import { components, hooks, utils } from '@generouted/react-router/client'
 
 export type Path =
   | `/`
+  | `/debug/expression`
   | `/debug/illuminance`
   | `/debug/menu`
   | `/debug/planes`
