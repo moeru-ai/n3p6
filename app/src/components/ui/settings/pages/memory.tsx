721123956ba915bc3a870d4da40ff1cc23dd57b0
藍+85CD <50108258+kwaa@users.noreply.github.com>
2025-08-20 19:59:24 +0800

    chore: add userId, agentId to memory setting

--- app/src/components/ui/settings/pages/memory.tsx
+++ app/src/components/ui/settings/pages/memory.tsx
@@ -8,7 +8,7 @@ import { useEffect, useState } from 'react'
 import { useSetMemuClient } from '~/context/memu-client'
 import { useMemuProvider } from '~/hooks/use-provider-memu'
 
-const MemuProvider = () => {
+export const SettingsMemory = (props: ContainerProperties) => {
   const [memuProvider, setMemuProvider] = useMemuProvider()
 
   const setMemuClient = useSetMemuClient()
@@ -16,8 +16,11 @@ const MemuProvider = () => {
   const [baseURL, setBaseURL] = useState(memuProvider.baseURL)
   const [apiKey, setApiKey] = useState(memuProvider.apiKey)
 
+  const [userId, setUserId] = useState(memuProvider.userId)
+  const [agentId, setAgentId] = useState(memuProvider.agentId)
+
   useEffect(() => setMemuClient(
-    memuProvider.baseURL
+    (memuProvider.baseURL && memuProvider.userId && memuProvider.agentId)
       ? new MemuClient({
         apiKey: memuProvider.apiKey,
         baseUrl: memuProvider.baseURL,
@@ -28,35 +31,33 @@ const MemuProvider = () => {
   ), [memuProvider, setMemuClient])
 
   return (
-    <Card height="auto">
-      <CardHeader>
-        <CardTitle>
-          <Text>Memory (MemU)</Text>
-        </CardTitle>
-        <CardDescription>
-          <Text>Your AI Memories.</Text>
-        </CardDescription>
-      </CardHeader>
-      <CardContent flexDirection="column" gap={16}>
-        <Input onValueChange={setBaseURL} placeholder="baseURL, e.g. https://api.memu.so" value={baseURL} />
-        <Input onValueChange={setApiKey} placeholder="apiKey (optional)" type="password" value={apiKey} />
-      </CardContent>
-      <CardFooter>
-        <Button
-          data-test-id="llm-provider-submit"
-          flexDirection="row"
-          onClick={() => setMemuProvider({ apiKey, baseURL })}
-          width="100%"
-        >
-          <Text>Submit</Text>
-        </Button>
-      </CardFooter>
-    </Card>
+    <Container flexDirection="column" gap={16} overflow="scroll" padding={16} {...props}>
+      <Card height="auto">
+        <CardHeader>
+          <CardTitle>
+            <Text>Memory (MemU)</Text>
+          </CardTitle>
+          <CardDescription>
+            <Text>Your AI Memories.</Text>
+          </CardDescription>
+        </CardHeader>
+        <CardContent flexDirection="column" gap={16}>
+          <Input onValueChange={setBaseURL} placeholder="baseURL, e.g. https://api.memu.so" value={baseURL} />
+          <Input onValueChange={setApiKey} placeholder="apiKey (optional)" type="password" value={apiKey} />
+          <Input onValueChange={setUserId} placeholder="userId, e.g. user01" value={userId} />
+          <Input onValueChange={setAgentId} placeholder="agentId, e.g. agent47" value={agentId} />
+        </CardContent>
+        <CardFooter>
+          <Button
+            data-test-id="memory-provider-submit"
+            flexDirection="row"
+            onClick={() => setMemuProvider({ agentId, apiKey, baseURL, userId })}
+            width="100%"
+          >
+            <Text>Submit</Text>
+          </Button>
+        </CardFooter>
+      </Card>
+    </Container>
   )
 }
-
-export const SettingsMemory = (props: ContainerProperties) => (
-  <Container flexDirection="column" gap={16} overflow="scroll" padding={16} {...props}>
-    <MemuProvider />
-  </Container>
-)
